fcc2277749d2ef0d7b2d015d082dbc5d352fa593
Groshan67 <ghassemroshan@gmail.com>
2023-11-14 20:31:40 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Container } from 'react-bootstrap';
 
-const App = () => {
+const App = (sd) => {
   return (
     <div>
       <Header />
